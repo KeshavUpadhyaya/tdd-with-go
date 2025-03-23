3537c44a15782220b29eae8f328948a9c9910b20
Keshav Upadhyaya <31659743+KeshavUpadhyaya@users.noreply.github.com>
2025-03-23 19:49:50 +0100

    structs, interfaces, methods, table driven tests

---
+++ structs/shapes_test.go
@@ -0,0 +1,62 @@
+package main
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
+	want := 40.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+
+func TestArea(t *testing.T) {
+
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	}
+
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12, 6}
+		checkArea(t, rectangle, 72.0)
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		checkArea(t, circle, 314.1592653589793)
+	})
+}
+
+// table driven tests
+func TestArea2(t *testing.T) {
+
+	areaTests := []struct {
+		name  string
+		shape Shape
+		want  float64
+	}{
+		// named fields! better for readability
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, want: 72.0},
+		{name: "Circle", shape: Circle{10}, want: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{12, 6}, want: 36.0},
+	}
+
+	for _, tt := range areaTests {
+
+		// go test -run TestArea2/Rectangle <- to run a specific named test!
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.want {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.want)
+			}
+		})
+
+	}
+
+}

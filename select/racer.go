4a4719e387b3eaabc95cce946372aeb4ede076d6
Keshav Upadhyaya <31659743+KeshavUpadhyaya@users.noreply.github.com>
2025-04-21 18:16:28 +0200

    select, httptest

---
+++ select/racer.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+// return named for documentation purposes
+// func Racer(a, b string) (winner string) {
+// 	aDuration := measureResponseTime(a)
+// 	bDuration := measureResponseTime(b)
+
+// 	if aDuration < bDuration {
+// 		return a
+// 	}
+
+// 	return b
+// }
+
+// func measureResponseTime(url string) time.Duration {
+// 	start := time.Now()
+// 	http.Get(url)
+// 	return time.Since(start)
+// }
+
+var tenSecondTimeout = 10 * time.Second
+
+func Racer(a, b string) (winner string, error error) {
+	return ConfigurableRacer(a, b, tenSecondTimeout)
+}
+
+func ConfigurableRacer(a, b string, timeout time.Duration) (winner string, error error) {
+	select {
+	case <-ping(a):
+		return a, nil
+	case <-ping(b):
+		return b, nil
+	case <-time.After(timeout):
+		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
+	}
+}
+
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+
+	return ch
+}
+
+// Note:
+// Always make channels
+
+// Notice how we have to use make when creating
+// a channel; rather than say var ch chan struct{}.
+// When you use var the variable will be initialised
+// with the "zero" value of the type.
+// So for string it is "", int it is 0, etc.
+// For channels the zero value is nil
+// and if you try and send to it with <- it
+// will block forever because you cannot send to nil channels
